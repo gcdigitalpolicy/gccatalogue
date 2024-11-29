2f38506acedadadd6897e84052140332347571dd
Kyle Fletcher <kyle.fletcher@gmail.com>
2024-11-29 11:55:41 -0500

    feat: update Footer contextual links to include Contact Us and Report an issue

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,17 +5,17 @@ import { GcdsFooter } from "@cdssnc/gcds-components-react-ssr";
 import { GcdsWrapper } from "@cdssnc/gcds-components-react-ssr/client";
 
 export const Footer = () => {
+
   const contextualLinks = JSON.stringify({
-    Plants: "/plants",
-    Pests: "#",
-    Seeds: "#",
+    "Contact Us": "#",
+    "Report an issue": "#"
   });
 
   return (
     <GcdsWrapper>
       <GcdsFooter
         display="full"
-        contextualHeading="Additional content"
+        contextualHeading="GC Digital Policy"
         contextualLinks={contextualLinks}
       />
     </GcdsWrapper>
